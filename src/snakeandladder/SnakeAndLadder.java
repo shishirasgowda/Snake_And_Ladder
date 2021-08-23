8f0ad7fa976769e0c8a4bc3c284db2b89d30d8a7
SHISHIRA <Shishiras98@gmail.com>
2021-08-23 11:21:35 +0530

    Initialized startpoint as 0 and printing it

---
+++ src/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package snakeandladder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args)
+	{
+		int startPosition = 0;
+		System.out.println("Starting position is" + " " + startPosition );
+	}
+}
